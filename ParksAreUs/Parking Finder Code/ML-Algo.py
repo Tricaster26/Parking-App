c9c79775bd089d5558505bdbb8805d378be666cb
Tricaster26 <s2088639@ed.ac.uk>
2023-06-14 13:22:29 +0100

    Training and Val data, set

--- ParksAreUs/Parking Finder Code/ML-Algo.py
+++ ParksAreUs/Parking Finder Code/ML-Algo.py
@@ -1,4 +1,26 @@
-import numpy as np
-import scipy as sc
-import sklearn as sk
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+image_size = (180, 180)
+batch_size = 128
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "ParksAreUs/Parking Finder Code/Parking_Dataset/test",
+    #only have one class
+    labels=None,
+    # entire folder is our set, no need to split
+    validation_split=None,
+    seed=123,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "ParksAreUs/Parking Finder Code/Parking_Dataset/valid",
+    labels=None,
+    validation_split=None,
+    seed=123,
+    image_size=image_size,
+    batch_size=batch_size,
+)
