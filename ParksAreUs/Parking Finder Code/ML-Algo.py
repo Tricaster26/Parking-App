8a54444bad07e79974c393ca2657868147267d07
Tricaster26 <s2088639@ed.ac.uk>
2023-06-14 14:01:19 +0100

    plot of images from train set shown

--- ParksAreUs/Parking Finder Code/ML-Algo.py
+++ ParksAreUs/Parking Finder Code/ML-Algo.py
@@ -34,5 +34,12 @@ aug_ds = keras.Sequential([
     layers.RandomFlip("horizontal_and_vertical"),
 ])
 
+plt.figure(figsize=(10, 10))
+for images in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.axis("off")
+
+
 def model:
-    
